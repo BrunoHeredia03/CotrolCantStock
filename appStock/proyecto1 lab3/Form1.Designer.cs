1442c33cc841a2e5054202fb81cdf22b94528a24
BrunoHeredia03 <iesbrunoheredia@gmail.com>
2024-09-28 00:10:28 -0300

    Add files via upload

---
+++ appStock/proyecto1 lab3/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace proyecto1_lab3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.productosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.agregarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.modificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.categoriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txtNombre = new System.Windows.Forms.TextBox();
+			this.txtCodigo = new System.Windows.Forms.TextBox();
+			this.cmbCategoria = new System.Windows.Forms.ComboBox();
+			this.btnbuscar = new System.Windows.Forms.Button();
+			this.dataGridViewProductos = new System.Windows.Forms.DataGridView();
+			this.btnagregar = new System.Windows.Forms.Button();
+			this.btnmodificar = new System.Windows.Forms.Button();
+			this.btneliminar = new System.Windows.Forms.Button();
+			this.lblAlerta = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.txtStock = new System.Windows.Forms.TextBox();
+			this.txtDescripcion = new System.Windows.Forms.TextBox();
+			this.txtPrecio = new System.Windows.Forms.TextBox();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewProductos)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.productosToolStripMenuItem,
+            this.categoriaToolStripMenuItem,
+            this.reportesToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// archivoToolStripMenuItem
+			// 
+			this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+			this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+			this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+			this.archivoToolStripMenuItem.Text = "Archivo";
+			// 
+			// salirToolStripMenuItem
+			// 
+			this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+			this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+			this.salirToolStripMenuItem.Text = "Salir";
+			// 
+			// productosToolStripMenuItem
+			// 
+			this.productosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarToolStripMenuItem,
+            this.modificarToolStripMenuItem,
+            this.eliminarToolStripMenuItem});
+			this.productosToolStripMenuItem.Name = "productosToolStripMenuItem";
+			this.productosToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+			this.productosToolStripMenuItem.Text = "Productos";
+			// 
+			// agregarToolStripMenuItem
+			// 
+			this.agregarToolStripMenuItem.Name = "agregarToolStripMenuItem";
+			this.agregarToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+			this.agregarToolStripMenuItem.Text = "Agregar";
+			// 
+			// modificarToolStripMenuItem
+			// 
+			this.modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
+			this.modificarToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+			this.modificarToolStripMenuItem.Text = "Modificar";
+			// 
+			// eliminarToolStripMenuItem
+			// 
+			this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+			this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+			this.eliminarToolStripMenuItem.Text = "Eliminar";
+			// 
+			// categoriaToolStripMenuItem
+			// 
+			this.categoriaToolStripMenuItem.Name = "categoriaToolStripMenuItem";
+			this.categoriaToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+			this.categoriaToolStripMenuItem.Text = "Categorias";
+			// 
+			// reportesToolStripMenuItem
+			// 
+			this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+			this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+			this.reportesToolStripMenuItem.Text = "Reportes";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 33);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Panel de Busqueda";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 62);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Nombre";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(14, 95);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(40, 13);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Codigo";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(224, 127);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(52, 13);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Categoria";
+			// 
+			// txtNombre
+			// 
+			this.txtNombre.Location = new System.Drawing.Point(72, 55);
+			this.txtNombre.Name = "txtNombre";
+			this.txtNombre.Size = new System.Drawing.Size(100, 20);
+			this.txtNombre.TabIndex = 5;
+			// 
+			// txtCodigo
+			// 
+			this.txtCodigo.Location = new System.Drawing.Point(72, 88);
+			this.txtCodigo.Name = "txtCodigo";
+			this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+			this.txtCodigo.TabIndex = 6;
+			// 
+			// cmbCategoria
+			// 
+			this.cmbCategoria.FormattingEnabled = true;
+			this.cmbCategoria.Location = new System.Drawing.Point(293, 125);
+			this.cmbCategoria.Name = "cmbCategoria";
+			this.cmbCategoria.Size = new System.Drawing.Size(121, 21);
+			this.cmbCategoria.TabIndex = 7;
+			this.cmbCategoria.SelectedIndexChanged += new System.EventHandler(this.cmbCategoria_SelectedIndexChanged);
+			// 
+			// btnbuscar
+			// 
+			this.btnbuscar.Location = new System.Drawing.Point(495, 78);
+			this.btnbuscar.Name = "btnbuscar";
+			this.btnbuscar.Size = new System.Drawing.Size(100, 30);
+			this.btnbuscar.TabIndex = 8;
+			this.btnbuscar.Text = "Buscar";
+			this.btnbuscar.UseVisualStyleBackColor = true;
+			this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click_1);
+			// 
+			// dataGridViewProductos
+			// 
+			this.dataGridViewProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewProductos.Location = new System.Drawing.Point(16, 164);
+			this.dataGridViewProductos.Name = "dataGridViewProductos";
+			this.dataGridViewProductos.Size = new System.Drawing.Size(644, 150);
+			this.dataGridViewProductos.TabIndex = 9;
+			this.dataGridViewProductos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.Eleccion);
+			// 
+			// btnagregar
+			// 
+			this.btnagregar.Location = new System.Drawing.Point(53, 335);
+			this.btnagregar.Name = "btnagregar";
+			this.btnagregar.Size = new System.Drawing.Size(140, 40);
+			this.btnagregar.TabIndex = 10;
+			this.btnagregar.Text = "Agregar";
+			this.btnagregar.UseVisualStyleBackColor = true;
+			this.btnagregar.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// btnmodificar
+			// 
+			this.btnmodificar.Location = new System.Drawing.Point(268, 335);
+			this.btnmodificar.Name = "btnmodificar";
+			this.btnmodificar.Size = new System.Drawing.Size(140, 40);
+			this.btnmodificar.TabIndex = 11;
+			this.btnmodificar.Text = "Modificar";
+			this.btnmodificar.UseVisualStyleBackColor = true;
+			this.btnmodificar.Click += new System.EventHandler(this.btnmodificar_Click);
+			// 
+			// btneliminar
+			// 
+			this.btneliminar.Location = new System.Drawing.Point(484, 335);
+			this.btneliminar.Name = "btneliminar";
+			this.btneliminar.Size = new System.Drawing.Size(140, 40);
+			this.btneliminar.TabIndex = 12;
+			this.btneliminar.Text = "Eliminar";
+			this.btneliminar.UseVisualStyleBackColor = true;
+			this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+			// 
+			// lblAlerta
+			// 
+			this.lblAlerta.AutoSize = true;
+			this.lblAlerta.BackColor = System.Drawing.SystemColors.Highlight;
+			this.lblAlerta.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.lblAlerta.Location = new System.Drawing.Point(274, 404);
+			this.lblAlerta.Name = "lblAlerta";
+			this.lblAlerta.Size = new System.Drawing.Size(125, 13);
+			this.lblAlerta.TabIndex = 13;
+			this.lblAlerta.Text = "Producto con Stock bajo";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(224, 62);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(35, 13);
+			this.label6.TabIndex = 14;
+			this.label6.Text = "Stock";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(224, 95);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(63, 13);
+			this.label7.TabIndex = 15;
+			this.label7.Text = "Descripcion";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(14, 127);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(37, 13);
+			this.label8.TabIndex = 16;
+			this.label8.Text = "Precio";
+			// 
+			// txtStock
+			// 
+			this.txtStock.Location = new System.Drawing.Point(293, 59);
+			this.txtStock.Name = "txtStock";
+			this.txtStock.Size = new System.Drawing.Size(100, 20);
+			this.txtStock.TabIndex = 17;
+			// 
+			// txtDescripcion
+			// 
+			this.txtDescripcion.Location = new System.Drawing.Point(293, 92);
+			this.txtDescripcion.Name = "txtDescripcion";
+			this.txtDescripcion.Size = new System.Drawing.Size(100, 20);
+			this.txtDescripcion.TabIndex = 18;
+			// 
+			// txtPrecio
+			// 
+			this.txtPrecio.Location = new System.Drawing.Point(72, 125);
+			this.txtPrecio.Name = "txtPrecio";
+			this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+			this.txtPrecio.TabIndex = 19;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.txtPrecio);
+			this.Controls.Add(this.txtDescripcion);
+			this.Controls.Add(this.txtStock);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.lblAlerta);
+			this.Controls.Add(this.btneliminar);
+			this.Controls.Add(this.btnmodificar);
+			this.Controls.Add(this.btnagregar);
+			this.Controls.Add(this.dataGridViewProductos);
+			this.Controls.Add(this.btnbuscar);
+			this.Controls.Add(this.cmbCategoria);
+			this.Controls.Add(this.txtCodigo);
+			this.Controls.Add(this.txtNombre);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.menuStrip1);
+			this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewProductos)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem productosToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem agregarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem modificarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem categoriaToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem reportesToolStripMenuItem;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox txtNombre;
+		private System.Windows.Forms.TextBox txtCodigo;
+		private System.Windows.Forms.ComboBox cmbCategoria;
+		private System.Windows.Forms.Button btnbuscar;
+		private System.Windows.Forms.DataGridView dataGridViewProductos;
+		private System.Windows.Forms.Button btnagregar;
+		private System.Windows.Forms.Button btnmodificar;
+		private System.Windows.Forms.Button btneliminar;
+		private System.Windows.Forms.Label lblAlerta;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox txtStock;
+		private System.Windows.Forms.TextBox txtDescripcion;
+		private System.Windows.Forms.TextBox txtPrecio;
+	}
+}
+
---
+++ appStock/proyecto1 lab3/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.OleDb;
+
+
+namespace proyecto1_lab3
+{
+	public partial class Form1 : Form
+	{
+		private string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=BdStock\DbStock1.accdb";
+		private OleDbConnection conexion = new OleDbConnection();
+
+		private OleDbCommand comando = new OleDbCommand();
+		 
+		private OleDbDataAdapter adaptador = new OleDbDataAdapter();
+		private string cadenaConexion = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=BdStock\DbStock1.accdb";
+		
+			public Form1()
+			{
+				InitializeComponent();
+				ProbarConexion();
+			
+			}
+
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			cmbCategoria.Items.Clear();
+			cmbCategoria.Items.Add("Prueba");
+
+			CargarProductos();
+		}
+
+
+
+		//Conexion y Prueba de conexion 
+		public void conexiones()
+			{
+				//recibe la cadena de conexion
+				conexion.ConnectionString = cadenaConexion;
+				conexion.Open();
+
+				// Asocia el comando SQL a la conexión y define el tipo de comando (SQL)
+				comando.Connection = conexion;
+				comando.CommandType = CommandType.Text;
+
+			}
+			public void ProbarConexion()
+			{
+				try
+				{
+					// Configurar la cadena de conexión
+					conexion.ConnectionString = cadenaConexion;
+					conexion.Open();
+					MessageBox.Show("Conexión a la base de datos exitosa.");
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Error al conectar con la base de datos: " + ex.Message);
+				}
+				finally
+				{
+					// Cerrar la conexión si está abierta
+					if (conexion.State == ConnectionState.Open)
+					{
+						conexion.Close();
+					}
+				}
+			}
+
+			private void button2_Click(object sender, EventArgs e)
+			{
+
+			using (OleDbConnection conexion = new OleDbConnection(connectionString))
+			{
+				try
+				{
+					string consulta = "INSERT INTO Tabla2 (Codigo, Nombre, Descripcion, Precio, Stock, Categoria) VALUES (@codigo, @nombre, @descripcion, @precio, @stock, @categoria)";
+					OleDbCommand comando = new OleDbCommand(consulta, conexion);
+
+					comando.Parameters.AddWithValue("@codigo", txtCodigo.Text);
+					comando.Parameters.AddWithValue("@nombre", txtNombre.Text);
+					comando.Parameters.AddWithValue("@descripcion", txtDescripcion.Text);
+					comando.Parameters.AddWithValue("@precio", decimal.Parse(txtPrecio.Text));
+					comando.Parameters.AddWithValue("@stock", int.Parse(txtStock.Text));
+					comando.Parameters.AddWithValue("@categoria", cmbCategoria.SelectedItem.ToString());
+
+					conexion.Open();
+					comando.ExecuteNonQuery();
+					conexion.Close();
+
+					
+					CargarProductos();
+					VerificarStockBajo();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Error al insertar el producto: " + ex.Message);
+				}
+			}
+
+
+		}
+			private void CargarProductos()
+
+		{
+			try
+			{
+				// Reinicia el DataSource del DataGridView antes de cargar nuevos datos
+				dataGridViewProductos.DataSource = null;
+
+				// Usamos la conexión en un bloque using para asegurar su correcto cierre
+				using (OleDbConnection conexion = new OleDbConnection(connectionString))
+				{
+					conexion.Open();
+
+					// Consulta SQL para obtener los productos
+					string consulta = "SELECT Codigo, Nombre, Descripcion, Precio, Stock, Categoria FROM Tabla2";
+
+					// Usamos un bloque using para el adaptador
+					using (OleDbDataAdapter adaptador = new OleDbDataAdapter(consulta, conexion))
+					{
+						// Crear un DataSet para almacenar los datos
+						DataSet ds = new DataSet();
+
+						// Llenar el DataSet con los datos obtenidos
+						adaptador.Fill(ds, "Tabla2");
+
+						// Configuramos el DataGridView con los datos del DataSet
+						dataGridViewProductos.DataSource = ds.Tables["Tabla2"];
+					}
+
+					// Ajustamos el tamaño de las columnas automáticamente
+					dataGridViewProductos.AutoResizeColumns();
+
+					// Configuramos algunas propiedades del DataGridView si es necesario
+					dataGridViewProductos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+					dataGridViewProductos.AllowUserToAddRows = false;  // No permitir filas vacías
+				}
+			}
+			catch (OleDbException dbEx)
+			{
+				// Error relacionado con la base de datos (conexión o consulta)
+				MessageBox.Show("Error al acceder a la base de datos: " + dbEx.Message);
+			}
+			catch (Exception ex)
+			{
+				// Otro tipo de error (general)
+				MessageBox.Show("Error al cargar los productos: " + ex.Message);
+			}
+		}
+
+
+
+
+
+		//00000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+
+		//0101010101010101010111111111111111111111111111111111111111111111111111111111111111
+
+
+		private void btnmodificar_Click(object sender, EventArgs e)
+		{
+
+			if (dataGridViewProductos.SelectedRows.Count > 0)
+			{
+				string codigo = dataGridViewProductos.SelectedRows[0].Cells["Codigo"].Value.ToString();
+
+				using (OleDbConnection conexion = new OleDbConnection(connectionString))
+				{
+					string consulta = "UPDATE Tabla2 SET Nombre=@nombre, Descripcion=@descripcion, Precio=@precio, Stock=@stock, Categoria=@categoria WHERE Codigo=@codigo";
+					OleDbCommand comando = new OleDbCommand(consulta, conexion);
+
+					comando.Parameters.AddWithValue("@nombre", txtNombre.Text);
+					comando.Parameters.AddWithValue("@descripcion", txtDescripcion.Text);
+					comando.Parameters.AddWithValue("@precio", decimal.Parse(txtPrecio.Text));
+					comando.Parameters.AddWithValue("@stock", int.Parse(txtStock.Text));
+					comando.Parameters.AddWithValue("@categoria", cmbCategoria.SelectedItem.ToString());
+					comando.Parameters.AddWithValue("@codigo", codigo);
+
+					conexion.Open();
+					comando.ExecuteNonQuery();
+					conexion.Close();
+
+					CargarProductos(); // Refrescar el DataGridView
+				}
+			}
+			else
+			{
+				MessageBox.Show("Seleccione un producto a modificar");
+			}
+		}
+
+		//}
+
+		private void btneliminar_Click(object sender, EventArgs e)
+		{
+
+			if (dataGridViewProductos.SelectedRows.Count > 0)
+			{
+				string codigo = dataGridViewProductos.SelectedRows[0].Cells["Codigo"].Value.ToString();
+
+				using (OleDbConnection conexion = new OleDbConnection(connectionString))
+				{
+					string consulta = "DELETE FROM Tabla2 WHERE Codigo=@codigo";
+					OleDbCommand comando = new OleDbCommand(consulta, conexion);
+					comando.Parameters.AddWithValue("@codigo", codigo);
+
+					conexion.Open();
+					comando.ExecuteNonQuery();
+					conexion.Close();
+
+					CargarProductos(); // Refrescar el DataGridView
+				}
+			}
+			else
+			{
+				MessageBox.Show("Seleccione un producto a eliminar");
+			}
+		}
+
+		//}
+
+		private void btnbuscar_Click(object sender, EventArgs e)
+		{
+
+			string consulta = "SELECT * FROM Productos WHERE 1=1"; // Inicialmente no tiene filtro
+
+			if (!string.IsNullOrEmpty(txtNombre.Text))
+				consulta += $" AND Nombre LIKE '%{txtNombre.Text}%'";
+
+			if (!string.IsNullOrEmpty(txtCodigo.Text))
+				consulta += $" AND Codigo LIKE '%{txtCodigo.Text}%'";
+
+			if (cmbCategoria.SelectedItem != null)
+				consulta += $" AND Categoria = '{cmbCategoria.SelectedItem.ToString()}'";
+
+			dataGridViewProductos.DataSource = EjecutarConsulta(consulta); // Refrescar con los datos filtrados
+
+
+		}
+
+		private DataTable EjecutarConsulta(string consulta)
+		{
+			// Crear una tabla en la que se almacenarán los datos resultantes
+			DataTable dt = new DataTable();
+
+			try
+			{
+				// Usar la conexión con un bloque using para asegurar su correcto cierre
+				using (OleDbConnection conexion = new OleDbConnection(connectionString))
+				{
+					// Abrir la conexión
+					conexion.Open();
+
+					// Crear un adaptador para ejecutar la consulta y llenar el DataTable
+					using (OleDbDataAdapter adaptador = new OleDbDataAdapter(consulta, conexion))
+					{
+						// Llenar el DataTable con los datos obtenidos de la consulta
+						adaptador.Fill(dt);
+					}
+				}
+			}
+			catch (OleDbException dbEx)
+			{
+				MessageBox.Show("Error al ejecutar la consulta: " + dbEx.Message);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Ocurrió un error: " + ex.Message);
+			}
+
+			// Devolver la tabla llena con los datos
+			return dt;
+		}
+
+
+
+		private void VerificarStockBajo()
+		{
+			string consulta = "SELECT * FROM Tabla2 WHERE Stock < 10";
+			DataTable productosBajoStock = EjecutarConsulta(consulta);
+
+			if (productosBajoStock.Rows.Count > 0)
+			{
+				lblAlerta.Text = "Producto con Stock bajo"; // Muestra un mensaje en la etiqueta
+				MessageBox.Show("Verificar Producto! Cuenta con stock bajo.");
+				lblAlerta.Visible = true;
+			}
+			else
+			{
+				lblAlerta.Visible = false;
+			}
+		}
+
+		private void cmbCategoria_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void Eleccion(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+
+		private void btnbuscar_Click_1(object sender, EventArgs e)
+		
+			
+			{
+				string consulta = "SELECT * FROM Tabla2 WHERE 1=1"; // Inicialmente no tiene filtro
+
+				// Agregar filtros según los valores ingresados
+				if (!string.IsNullOrEmpty(txtNombre.Text))
+					consulta += $" AND Nombre LIKE '%{txtNombre.Text}%'";
+
+				if (!string.IsNullOrEmpty(txtCodigo.Text))
+					consulta += $" AND Codigo LIKE '%{txtCodigo.Text}%'";
+
+				if (cmbCategoria.SelectedItem != null)
+					consulta += $" AND Categoria = '{cmbCategoria.SelectedItem.ToString()}'";
+
+				// Ejecutar la consulta y actualizar la grilla con los resultados
+				dataGridViewProductos.DataSource = EjecutarConsulta(consulta); // Refrescar con los datos filtrados
+			}
+
+		}
+	}
+
+
+
